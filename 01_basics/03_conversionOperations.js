b2fff4fb71f1bf1da2a12bd4bb71dc8fbd1fd68d
samad1309 <116417823+samad1309@users.noreply.github.com>
2024-06-03 15:06:57 +0000

    learnt type conversion

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,31 @@
+let score = "samad"
+
+console.log(typeof(score));
+
+// String to NUmber
+let valueInNumber = Number(score)
+console.log(typeof(valueInNumber))
+console.log(valueInNumber)
+// Similarly
+// "33" => 33
+// "33abc" => NaN
+// true => 1 false =>0
+
+
+// " " => false "notemptystring" => true
+nam = "Ken"
+let inBoolean = Boolean(nam)
+console.log(inBoolean)
+console.log(typeof(inBoolean))
+
+// Number to String
+let isNum = 13
+let isString = String(isNum)
+
+console.log(isString)
+console.log(typeof(isString))
+
+console.log(isString + 3 + 4)
+console.log(3 + 4 + isString)
+
+// Learn about post increment and preincrement
